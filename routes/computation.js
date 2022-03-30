86a803c52e4acff336e1d0ef43f8fb77b2495e65
Rohith-Chittipolu <98173410+Rohith-Chittipolu@users.noreply.github.com>
2022-03-30 13:24:37 -0500

    bonus

--- routes/computation.js
+++ routes/computation.js
@@ -1,17 +1,16 @@
 var express = require('express');
+const { param } = require('./users');
 var router = express.Router();
-var m = Math.random();
+var m = Math.random().toFixed(2);
 
 
 console.log(m);
-
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
-
+  if (req.query.m != undefined) {
+    m = req.query.m;
+  }
+   
   res.render('computation', { Calculate: 'Cos of ' +m+' is '+Math.cos(m) });
- 
-
 });
-
 module.exports = router;
